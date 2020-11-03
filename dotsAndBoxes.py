365a2015e967d775756eb7b2eec7514d661b931a
cwyz-dev <chris@wwwoods.xyz>
2020-11-02 22:25:56 -0700

    Filled out a basic outline of my functions, more testing needed once Board is more complete.

--- dotsAndBoxes.py
+++ dotsAndBoxes.py
@@ -2,21 +2,35 @@
 #   self.board = Board(x,y) from board.py
 #   self.depth = depth of search
 
+import sys
+
+import board
+import box
+
 class DotsAndBoxes:
     # Initializer
     def __init__(self,x,y):
         # TODO: Chris
-        return
+        self.board = board.Board(x,y)
 
     # The game loop
     def play(self):
         # TODO: Chris
-        return
+        self.board.displayBoard()
+        while (len(self.board.available_moves) > 0):
+            self.playerTurn(self)
+        self.getWinner(self)
 
     # Handles user input
     def playerTurn(self):
         # TODO: Chris
-        return
+        move = input ('Enter your move ([Pt1] [Pt2]): ')
+        moves = move.split(' ')
+        while (len(moves) != 2):
+            move = input('Please enter moves according to the form [x1,y1] [x2,y2]: ')
+            moves = move.split(' ')
+        self.board.connectDots([moves[0], moves[1]], 'Human')
+        self.board.displayBoard()
 
     # Determine who won, print out final state and relevant data
     def getWinner(self):
@@ -26,4 +40,8 @@ class DotsAndBoxes:
 # Main running order of the code
 if __name__ == '__main__':
     # TODO: Chris
-    print('Running')
+    if (len(sys.argv) < 3):
+        print('Please call this file as \'python3 dotsAndBoxes.py [X] [Y]')
+    else:
+        dab = DotsAndBoxes(sys.argv[1], sys.argv[2])
+        dab.play()
