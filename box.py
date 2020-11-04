a2f489b5fff06bc438f18bd0da24148f56253676
paola4 <arevalo@ualberta.ca>
2020-11-03 21:27:45 -0700

    Update box.py

--- box.py
+++ box.py
@@ -8,6 +8,16 @@ class Box:
     def __init__(self, x, y):
         # x,y are coordinates for the for top left corner
 
+        # Tracking if box is complete and which player completed it
+        self.complete  = False 
+        self.filled_by = None # Player 0 = AI, 1 = Person
+
+        # Tracks if a line is filled
+        self.top_done    = False
+        self.left_done   = False
+        self.bottom_done = False
+        self.right_done  = False
+
         #Top-Left coordinate represented as tuple
         self.top_left = (x,y)
 
@@ -23,38 +33,31 @@ class Box:
         # Array with all four lines' coordinates
         self.lines = [self.topline, self.leftline, self.rightline, self.bottomline]
 
-        # Tracks if a line is filled
-        self.top_done = False
-        self.left_done = False
-        self.bottom_done = False
-        self.right_done = False
-
-        # Tracking if box is complete and which player completed it
-        self.complete = False 
-        self.filled_by = None # Player 0 = AI, 1 = Person
-
     def connect(self, coordinates):
             # Check if given coordinates form a line of the current box.
             # - Coordinates is a tuple of a pair of tuples ((x1,y1),(x2,y2)) representing a line
             # - Returns a Bool True or False if a successful connection is made
 
-        connected = False # Boolean to return if coordinates connect as a line for this box
+        dots_connected = False # Boolean to return if coordinates connect as a line for this box
         if coordinates in self.lines:
-            if coordinates == self.topline and self.top_done == False
-                self.top_done = True
-                connected = True
-            elif coordinates == self.leftline and self.left_done == False
+            if   (coordinates == self.topline)    and (self.top_done == False):
+                self.top_done  = True
+                dots_connected = True
+                
+            elif (coordinates == self.leftline)   and (self.left_done == False):
                 self.left_done = True
-                connected = True
-            elif coordinates == self.bottomline and self.bottom_done == False
+                dots_connected = True
+
+            elif (coordinates == self.bottomline) and (self.bottom_done == False):
                 self.bottom_done = True
-                connected = True
-            elif coordinates == self.rightline and self.right_done == False
+                dots_connected   = True
+
+            elif (coordinates == self.rightline)  and (self.right_done == False):
                 self.right_done = True
-                connected = True
+                dots_connected  = True
 
         # Check if box is complete
         if self.top_done and self.left_done and self.bottom_done and self.right_done:
             self.complete = True
 
-        return connected
+        return dots_connected
