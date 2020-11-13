fc079a4615d18a40068d06ddac6337ab5cfab287
paola4 <arevalo@ualberta.ca>
2020-11-13 16:23:20 -0700

    Figuring out miniMax() function
    adjusting "best_score"

--- miniMax.py
+++ miniMax.py
@@ -13,37 +13,42 @@ class Minimax:
 
         # IF IT'S MAX TURN
         if (board.player == "Player 1"):
-            #alpha = -1000 # WC for MAX 
+            best_score = -1000 # WC for MAX 
             for child in board.children:
                 result = self.miniMax(child, depth - 1, alpha, beta)
-                if (result > alpha):
-                    alpha = result # Found a better best move
+                if (result > best_score):
+                    best_score = result # Found a better best move
+                if (best_score > alpha):
+                    alpha = best_score
                 # Check for pruning
                 if alpha >= beta:
-                    return alpha #Break early
-            board.game_score = alpha
-            return alpha 
+                    break
+                    #return alpha #Break early
+            board.game_score = best_score
+            return best_score
             
         # IF IT'S MIN TURN
         elif (board.player == "Player 2"):
-            #beta = 1000 # WC FOR MIN
+            best_score = 1000 # WC FOR MIN
             for child in board.children:
                 result = self.miniMax(child, depth - 1, alpha, beta)
-                if (result < beta):
-                    beta = result # Opponent has found a better worse move
+                if (result < best_score):
+                    best_score = result # Opponent has found a better worse move
+                if best_score < beta:
+                    beta = best_score
                 # Check for pruning
                 if beta <= alpha:
                     break
                     #return beta #Break early
-            board.game_score = beta
-            return beta
+            board.game_score = best_score
+            return best_score
 
-    def getMove(self, board, depth=10):
+    def getMove(self, board, depth=15):
         board = copy.deepcopy(board)
 
         # GENERATE SCORES
         best_score = self.miniMax(board, depth, -1000, 1000)
-
+        print('Best score: ' + str(best_score))
         # Iterate through children of current state to find best best move
         for child in board.children:
             if child.game_score == best_score:
